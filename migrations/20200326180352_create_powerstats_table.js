af94c10b7fe365c933a4235a80987e79e56b5cb8
Will Smith <wismith@davidson.edu>
2020-03-27 15:20:43 -0500

    edit migration

--- migrations/20200326180352_create_powerstats_table.js
+++ migrations/20200326180352_create_powerstats_table.js
@@ -1,3 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('powerstats', (table) => {
+    table.increments('id').primary();
+    table.integer('hero_id').references('heroes.id');
+    table.integer('intelligence');
+    table.integer('strength');
+    table.integer('speed');
+    table.integer('durability');
+    table.integer('power');
+    table.integer('combat');
+  })
+}
+
 exports.down = function(knex) {
   return knex.schema.dropTable('powerstats');
 }
